fe530dee6176d1c309d44261a5bda7da059f6983
HHB <33392861+764777472@users.noreply.github.com>
2022-07-21 15:37:38 +0800

    class

--- api/api.php
+++ api/api.php
@@ -2,14 +2,15 @@
 	/**
 	 * [setHeader 跨域设置]
 	 */
-	protected function setHeader()
-	{
-		header('Content-Type:application/json; charset=utf-8');
-		header("Access-Control-Allow-Origin:*");
-		header("Access-Control-Allow-Methods:POST,GET");
-	}
-	
-	public function getinfo() {
-		$this->setHeader();
-		echo "哈哈";
+	// protected function setHeader()
+	// {
+	// 	header('Content-Type:application/json; charset=utf-8');
+	// 	header("Access-Control-Allow-Origin:*");
+	// 	header("Access-Control-Allow-Methods:POST,GET");
+	// }
+	class base {
+		public function getinfo() {
+			// $this->setHeader();
+			echo "echo text!";
+		}
 	}
